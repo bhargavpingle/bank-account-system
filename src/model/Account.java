bc5a6695d51f5ddfe44417e0e9ea8c7267b77c5c
bhargav <bhargavreddypingle@gmail.com>
2020-08-18 12:00:00 -0500

    Added Account model class with deposit and withdraw methods

---
+++ src/model/Account.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Account {
+    private String accountNumber;
+    private String holderName;
+    private double balance;
+
+    public Account(String accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    // Getters and setters for accountNumber, holderName, and balance
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && this.balance >= amount) {
+            this.balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
